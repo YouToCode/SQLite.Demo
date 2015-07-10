458c94f60227feff8d9c3641e02efe1d5a01d884
Point <w506044@163.com>
2015-07-10 17:40:56 +0800

    2015-07-10-04

--- HCLUtility/Properties/AssemblyInfo.cs
+++ HCLUtility/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("HCLUtility")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("HaoCL类库")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("HaoCL_王号")]
 [assembly: AssemblyProduct("HCLUtility")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
